6956d4d16530c9efc20f186f1210cea861681444
Yafet Surafel <wedeyafet@gmail.com>
2023-01-31 10:26:24 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xml_generator",
+    version="0.0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "frappe"
+    ],
+)
